088af2b80cff927d757b549a77191d47beb25b87
Matt Meents <mmeents@users.noreply.github.com>
2022-11-25 22:44:22 -0500

    Move to Net 6 target.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace dbWorkshop
-{
-  static class Program
-  {
+namespace DBWorkshop {
+  internal static class Program {
     /// <summary>
-    /// The main entry point for the application.
+    ///  The main entry point for the application.
     /// </summary>
     [STAThread]
-    static void Main()
-    {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
+    static void Main() {
+      // To customize application configuration such as set high DPI settings or default font,
+      // see https://aka.ms/applicationconfiguration.
+      ApplicationConfiguration.Initialize();
       Application.Run(new Form1());
     }
   }
